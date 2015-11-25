b00a2449c4e204d60db17836587bb3209f282ae2
meanprogrammer <mean.programmer.d@gmail.com>
2015-11-25 10:10:48 +0800

    ewewew

--- ObjectConverter.cs
+++ ObjectConverter.cs
@@ -42,7 +42,7 @@ namespace WebAppWalkthrough
                 cov.CovenantID = item.new_nsocovenantId.Value;
                 cov.Name = item.new_name;
                 cov.ParentID = (item.new_opportunity_new_nsocovenant != null) ? item.new_opportunity_new_nsocovenant.OpportunityId.Value : Guid.Empty;
-                cov.ParentIDString = cov.ParentID.ToString();
+                cov.ParentIDString = string.Format("{0}{1}", "__bo4200", cov.ParentID.ToString());
                 nsos.Add(cov);
             }
             return nsos;
